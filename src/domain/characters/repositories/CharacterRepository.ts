d72467b9fd006ad479d4bff3ea69887d9ab5f318
Nimna Avanth <avantha.nimna@gmail.com>
2025-04-23 10:29:41 +0530

    fetch data from the api

--- src/domain/characters/repositories/CharacterRepository.ts
+++ src/domain/characters/repositories/CharacterRepository.ts
@@ -1,29 +1,18 @@
 import CharacterCollection from "../collections/CharacterCollection";
 import CharacterRepositoryContract from "../contracts/CharacterRepositoryContract";
 import Character from "../models/character";
+import axios from "axios";
 
 class CharacterRepository implements CharacterRepositoryContract {
   async all(): Promise<CharacterCollection> {
-    // Return a mock character collection array
-    return new CharacterCollection([
-      new Character(
-        1,
-        "Rick Sanchez",
-        "Alive",
-        "Human",
-        "",
-        "Male",
-        { name: "Earth", url: "https://rickandmortyapi.com/api/location/1" },
-        { name: "Earth", url: "https://rickandmortyapi.com/api/location/20" },
-        "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-        [
-          "https://rickandmortyapi.com/api/episode/1",
-          "https://rickandmortyapi.com/api/episode/2",
-        ],
-        "https://rickandmortyapi.com/api/character/1",
-        "2017-11-04T18:48:46.250Z"
-      ),
-    ]);
+    const response = await axios.get(
+      "https://rickandmortyapi.com/api/character"
+    );
+    const data = response.data;
+
+    return new CharacterCollection(
+      data.results.map((character: Character) => character)
+    );
   }
 }
 
--- src/pages/characters/index.tsx
+++ src/pages/characters/index.tsx
@@ -22,25 +22,31 @@ function Index() {
     }, [])
 
     return (
-        <div>
-            <h1>Characters</h1>
-            {characters && characters.characters.map((character) => (
-            <Card
-                key={character.id}
-                name={character.name}
-                status={character.status}
-                species={character.species}
-                id={character.id}
-                type={character.type}
-                gender={character.gender}
-                origin={character.origin}
-                location={character.location}
-                image={character.image}
-                episode={character.episode}
-                url={character.url}
-                created={character.created}
-            />
-            ))}
+        <div className="p-6 m-4">
+            <div className="mb-8 space-y-2">
+              <h1 className='text-3xl text-black font-bold text-center'>Characters</h1>
+              <h2 className='text-xl text-gray-600 font-semibold text-center'>Rick and Morty</h2>
+              <div className="w-24 h-1 mx-auto bg-blue-500 rounded my-4"></div>
+            </div>
+            <div className="grid grid-cols-4 gap-4">
+                {characters && characters.characters.map((character) => (
+                    <Card
+                        key={character.id}
+                        name={character.name}
+                        status={character.status}
+                        species={character.species}
+                        id={character.id}
+                        type={character.type}
+                        gender={character.gender}
+                        origin={character.origin}
+                        location={character.location}
+                        image={character.image}
+                        episode={character.episode}
+                        url={character.url}
+                        created={character.created}
+                    />
+                ))}
+            </div>
         </div>
     )
 }
